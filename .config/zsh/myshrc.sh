6dd16291cdece8315beefcb1fbbe944f8772080a
qiekn <qiekn@kimidayo.cn>
2025-03-01 19:37:49 +0800

    add config
    zsh
    tmux
    yabai
    skhd
    espanso

--- .config/zsh/myshrc.sh
+++ .config/zsh/myshrc.sh
@@ -23,17 +23,10 @@ PATH=$PATH:/Users/leoua7/.nvm/versions/node/v12.14.0/lib/node_modules/hexo-cli/b
 PATH=$PATH:/usr/local/apache-maven-3.9.4/bin
 PATH=$PATH:/Users/leoua7/.local/bin # Created by `pipx` on 2023-10-12 06:02:49
 
-
 # cli-tools
 alias tdl="/Users/leoua7/Desktop/dev/telegram_dl/tdl"
 export TDL_NS=yaoyaoqiekn
 export TDL_PROXY=socks5://127.0.0.1:7890
-
-
-# @squoosh/cli
-alias tinyimg="squoosh-cli --mozjpeg '{"quality":75,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":80}'"
-
-## /*** cli-tools end ***/
 
 
 # -------------------------------------------
