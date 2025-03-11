0fd7a2c75a6df8eec3a2383b7c9ab6447660a9ad
qiekn <qiekn@kimidayo.cn>
2025-03-12 07:03:06 +0800

    copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,9 +1,11 @@
 return {
   {
+    --[[
     "github/copilot.vim",
     config = function()
       -- The free quota has been used up, temporarily disabled
       vim.g.copilot_enabled = false
     end,
+    --]]
   },
 }
