07e21d0206c74efda35a2c11c4f11d400e52238b
qiekn <qiekn@kimidayo.cn>
2025-03-12 06:15:41 +0800

    nvim lsp remove some commnet

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -218,16 +218,6 @@ return {
         },
         gopls = {},
         pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -283,7 +273,7 @@ return {
           end,
         },
       })
-    end,
+    end, -- config function end
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
