0728b4aef1c63c9c49d015061bb1abc4ba6fc8f4
qiekn <qiekn@kimidayo.cn>
2025-03-13 02:15:16 +0800

    nvim colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,49 @@
 -- [[Color Schemes]]
 -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 return {
+  --------------
+  --  Themes  --
+  --------------
+
   { "rose-pine/neovim" },
   { "projekt0n/github-nvim-theme" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "folke/tokyonight.nvim" },
-  {
+
+  --------------
+  --  Config  --
+  --------------
+
+  { -- Preview theme with telescope
+    "andrew-george/telescope-themes",
+    keys = {
+      { "<leader>st", "<cmd>Telescope themes<CR>", desc = "[S]et [T]hmes" },
+    },
+    config = function()
+      require("telescope").load_extension("themes")
+    end,
+  },
+  { -- Transparent background
     "xiyaowong/transparent.nvim",
     config = function()
       -- Load the colorscheme here.
       vim.cmd.colorscheme("github_dark")
       require("transparent").setup({
-        extra_groups = { "NeoTreeNormal" },
-        exclude_groups = { "CursorLine", "CursorLineNr", "NeoTreeCursorLine" },
+        groups = {
+          "Normal",
+          "NormalNC",
+          "NormalFloat",
+          "SignColumn",
+        },
+        extra_groups = {
+          "NeoTreeNormal",
+          "TelescopeNormal",
+          "TelescopeBorder",
+          -- "TelescopeSelection",
+          -- "TelescopeSelectionCaret",
+        },
       })
-      -- require("transparent").clear_prefix("NeoTree")
+      require("transparent").clear_prefix("Telescope")
       vim.cmd("TransparentEnable")
     end,
   },
