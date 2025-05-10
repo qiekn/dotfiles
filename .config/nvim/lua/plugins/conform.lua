65518a9d92547a088eb41b56d6b30dac434e6420
qiekn <qieknxxx@gmail.com>
2025-05-10 19:04:19 +0800

    conform clang-format

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -19,7 +19,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true }
+        local disable_filetypes = { c = true, json = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = "never"
@@ -34,19 +34,12 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         go = { "gofmt", "goimports" },
-        cpp = { "clang_format" },
+        cpp = { "clang-format" },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-      formatters = {
-        clang_format = {
-          exe = "clang-format",
-          args = { "--style=Google", "-assume-filename", "$FILENAME" },
-          stdin = true,
-        },
       },
     },
   },
