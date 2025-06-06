40149334ca83c13b42ae8036880a32fc46248cf9
qiekn <qieknxxx@gmail.com>
2025-06-07 03:25:16 +0800

    lua lsp config

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -237,10 +237,14 @@ return {
           settings = {
             Lua = {
               completion = {
-                callSnippet = "Replace",
+                callSnippet = "Disable",
+                showParams = false,
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               diagnostics = { disable = { "missing-fields" } },
+              hint = {
+                paramName = "Disable",
+              },
             },
           },
         },
