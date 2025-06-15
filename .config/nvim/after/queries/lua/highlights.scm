4fd2883dcf84b1f5da021a84e72e9dc9953cd774
qiekn <qieknxxx@gmail.com>
2025-06-15 21:07:14 +0800

    update lua conceal

--- .config/nvim/after/queries/lua/highlights.scm
+++ .config/nvim/after/queries/lua/highlights.scm
@@ -1,7 +1,9 @@
 ;; extends 
 
 (("end" @punctuation.bracket) (#set! conceal "}"))
-(("do" @punctuation.bracket) (#set! conceal "{"))
-(("and" @keyword.function) (#set! conceal "&"))
+((identifier) @variable.builtin (#eq? @variable.builtin "self") (#set! conceal ""))
+(("function" @keyword.function) (#set! conceal "󰊕"))
 (("return" @keyword.function) (#set! conceal "R"))
 (("then" @punctuation.bracket) (#set! conceal "{"))
+(("do" @punctuation.bracket) (#set! conceal "{"))
+(("local" @keyword) (#set! conceal ""))
