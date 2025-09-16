748340742970656a2831b20e82bdc4bcb67dec19
qiekn <qiekndev@gmail.com>
2025-09-17 02:47:33 +0800

    nvim: telescope: sc(show vim commands) <c-y>(live grep)

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -76,6 +76,7 @@ return {
             "deps[/\\]",
             "vendor[/\\]",
             "3rdparty[/\\]",
+            "lib[/\\]",
           },
         },
       })
@@ -92,12 +93,13 @@ return {
       vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
       vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
       vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-      vim.keymap.set("n", "<c-i>", builtin.live_grep)
+      vim.keymap.set("n", "<c-y>", builtin.live_grep)
       vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
       vim.keymap.set("n", "<leader>so", builtin.vim_options, { desc = "[S]et vim [O]ptions" })
+      vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "[S]earch vim [C]ommands" })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set("n", "<leader>/", function()
