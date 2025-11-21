eb797a3672a04b407eee1b3c674c04b2cfab6cc8
qiekn <qiekndev@gmail.com>
2025-11-22 03:28:42 +0800

    chore: mason disable gopls insure install (manually install go at first)

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -29,7 +29,7 @@ return {
         "clangd",
         "glsl_analyzer",
         -- Other languages
-        "gopls",
+        -- "gopls", -- install go at first
         "pyright",
         "json-lsp",
         -- Utils
