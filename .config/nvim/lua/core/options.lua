b37ed85287ed0ba91b8999c55129053064b67097
qiekn <qiekndev@gmail.com>
2025-12-20 09:29:47 +0800

    nvim: msys2 shellcmd

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -80,3 +80,9 @@ vim.opt.fileencodings = { "utf-8", "gb2312", "gbk", "gb18030" }
 vim.opt.fileencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileformat = "unix"
+
+-- Msys2 shell
+vim.opt.shellcmdflag = "-c"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
+vim.opt.shellslash = true
