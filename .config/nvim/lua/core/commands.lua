ced6e4c68054875d68ac71acb8219300598126f9
qiekn <qiekndev@gmail.com>
2025-12-20 09:29:38 +0800

    nvim: commands: code runner & toggle markdown render

--- .config/nvim/lua/core/commands.lua
+++ .config/nvim/lua/core/commands.lua
@@ -1,2 +1,14 @@
 vim.cmd("command Q q!")
 vim.cmd("command Symbols Neotree document_symbols")
+vim.cmd("command Rmd RenderMarkdown toggle")
+
+-- Code Runner
+vim.api.nvim_create_user_command("Run", function()
+  local ft = vim.bo.filetype
+
+  if ft == "javascript" then
+    vim.cmd("!node %:.")
+  else
+    print("Not support this filetype yet.")
+  end
+end, {})
