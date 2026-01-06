1e215bdab468ca141761cd1a36503c9b8880e0a2
qiekn <qiekndev@gmail.com>
2026-01-06 11:59:26 +0800

    nvim: code runner

--- .config/nvim/lua/core/commands.lua
+++ .config/nvim/lua/core/commands.lua
@@ -8,6 +8,13 @@ vim.api.nvim_create_user_command("Run", function()
 
   if ft == "javascript" then
     vim.cmd("!node %:.")
+  elseif ft == "cpp" then
+    local exe_name = vim.fn.expand("%:t:r")
+    local root = vim.fs.root(0, "CMakeLists.txt")
+    local build_dir = root .. "/build"
+    local exe_path = build_dir .. "/" .. exe_name
+
+    vim.cmd("!" .. "cmake --build " .. build_dir .. " --target " .. exe_name .. " -j && " .. exe_path)
   else
     print("Not support this filetype yet.")
   end
