6d683396a2adff39870ac9dfe7752a9c9698a4b7
qiekn <qiekndev@gmail.com>
2026-02-14 15:36:52 +0800

    msys2: windows terminal path sync

--- .config/zsh/msys2.zsh
+++ .config/zsh/msys2.zsh
@@ -19,7 +19,10 @@ if [[ -n "$MSYSTEM" || "$(uname -s)" =~ (MSYS|MINGW|UCRT|CYGWIN) ]]; then
   export PATH=$PATH:"/c/Apps/Go/bin"
   export PATH=$PATH:"/c/Users/user/AppData/Roaming/npm/"
   export PATH=$PATH:"/c/Apps/shortcuts/"
+  export PATH=$PATH:"$HOME/.local/share/pnpm"
   export PATH=$PATH:"$HOME/.local/bin/"
+  export PATH=$PATH:"$HOME/.local/bin/"
+  export PATH=$PATH:"$WINHOME/.local/bin/"
 
   # unset APPDATA
   # unset LOCALAPPDATA
@@ -45,9 +48,25 @@ if [[ -n "$MSYSTEM" || "$(uname -s)" =~ (MSYS|MINGW|UCRT|CYGWIN) ]]; then
   alias winhome="cd $WINHOME"
   alias desktop="cd $WINHOME/Desktop"
   alias emacs="/c/msys64/mingw64/bin/runemacs.exe"
-  alias code="winhome/"
   alias code="$WINHOME/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code" # vscode
 
   function windata() { cd $APPDATA }
+
+  # Fix tmux
+  function tmux() {
+    ps -ef | grep "[t]mux" > /dev/null || rm -f /tmp/tmux-$(id -u)/default 2>/dev/null
+    command tmux "$@"
+  }
+
+  # Command --> C:\msys64\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell zsh
+  # Windows Terminal current directory sync
+  if [[ -n "$WT_SESSION" ]]; then
+    function tell_wt_cwd() {
+      printf "\e]9;9;%s\e\\" "$(cygpath -w "$PWD")"
+    }
+
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd tell_wt_cwd
+  fi
 
 fi
