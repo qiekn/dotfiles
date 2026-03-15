5ed8602fa9a57489ae21bba39f8433022faafe8b
qiekn <qiekndev@gmail.com>
2026-03-16 06:09:50 +0800

    zsh: yt-dlp

--- .config/zsh/func.zsh
+++ .config/zsh/func.zsh
@@ -18,3 +18,20 @@ function mkcd {
     mkdir $@ && cd $last
   fi
 }
+
+# yt-dlp 快速截取片段函数
+vcut() {
+  local start=$1
+  local end=$2
+  local url=$3
+
+  if [[ -z "$url" || -z "$start" || -z "$end" ]]; then
+    echo "usage: vcut [URL] [start-time] [end-time]"
+    return 1
+  fi
+
+  yt-dlp --download-sections "*${start}-${end}" \
+    --force-keyframes-at-cuts \
+    -f "bv+ba/b" \
+    "$url"
+  }
