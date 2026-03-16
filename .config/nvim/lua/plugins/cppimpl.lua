7ef450982d47e63a3ae13406cc71773476a70d9e
qiekn <qiekndev@gmail.com>
2026-03-16 08:29:29 +0800

    nvim: plugin → cpp impl

--- .config/nvim/lua/plugins/cppimpl.lua
+++ .config/nvim/lua/plugins/cppimpl.lua
@@ -1,10 +1,20 @@
 return {
   {
-    -- in header file run `:Generate implementations`
-    "eriks47/generate.nvim",
+    "qiekn/generate.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      vim.api.nvim_create_user_command("CppImpl", "Generate implementations", {})
+    cmd = { "Generate", "CppImpl" },
+    keys = {
+      { "<leader>ci", "<cmd>Generate implementations<cr>", desc = "Generate C++ implementations", ft = { "cpp", "c" } },
+      { "<leader>ci", ":'<,'>Generate implementations<cr>", mode = "v", desc = "Generate selected implementations", ft = { "cpp", "c" } },
+    },
+    opts = {},
+    config = function(_, opts)
+      require("generate").setup(opts)
+      vim.api.nvim_create_user_command("CppImpl", function(o)
+        vim.cmd(o.range == 2
+          and (o.line1 .. "," .. o.line2 .. "Generate implementations")
+          or "Generate implementations")
+      end, { range = true })
     end,
   },
 }
