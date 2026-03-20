d13e9c195b32f02abae037bfc2a6fc1416b7d40b
qiekn <qiekndev@gmail.com>
2026-03-21 04:57:30 +0800

    nvim: style → fzf

--- .config/nvim/lua/core/scheme.lua
+++ .config/nvim/lua/core/scheme.lua
@@ -1,11 +1,19 @@
+local C = {
+  orange = "#ff9303",
+}
+
 vim.cmd("hi Normal ctermbg=none guibg=none")
 vim.cmd("hi CursorLine guibg=#383f58")
 -- vim.cmd("hi CursorLine guibg=red ctermbg=red ctermfg=white")
 -- vim.cmd("hi CursorLineNr guibg=red ctermbg=red ctermfg=white")
 
 vim.cmd("hi @EveryoneLoveDuck guifg=#ffa657")
-vim.cmd("hi FloatBorder guifg=#ff9303")
+vim.cmd("hi FloatBorder guifg=" .. C.orange)
+vim.cmd("hi FzfLuaFzfMatch guifg=" .. C.orange)
+vim.cmd("hi FzfLuaBorder guifg=" .. C.orange)
 
 -- Fix italic in LSP hover
 vim.cmd("hi @markup.raw gui=none")
 vim.cmd("hi @markup.italic gui=none")
+
+return C
--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,16 +1,60 @@
 return {
   "ibhagwan/fzf-lua",
-  -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "nvim-mini/mini.icons" },
-  ---@module "fzf-lua"
-  ---@type fzf-lua.Config|{}
-  ---@diagnostic disable: missing-fields
-  opts = {
-    defaults = {
-      header = false,
+  config = function()
+    local actions = require("fzf-lua.actions")
+    require("fzf-lua").setup({
+      defaults = {
+        header = false,
+        formatter = "path.filename_first",
+      },
+      winopts = {
+        title_flags = false,
+        preview = { hidden = "hidden" },
+        scrollbar = false,
+      },
+      fzf_colors = {
+        ["hl"] = { "fg", "FzfLuaFzfMatch" },
+        ["hl+"] = { "fg", "FzfLuaFzfMatch" },
+        ["bg+"] = { "bg", "CursorLine" },
+        ["fg+"] = { "fg", "Normal" },
+        ["separator"] = { "fg", "FzfLuaBorder" },
+        ["scrollbar"] = { "fg", "FzfLuaBorder" },
+      },
+      fzf_opts = {
+        -- ["--separator"] = "─",
+        ["--separator"] = " ",
+      },
+      actions = {
+        files = {
+          ["enter"] = actions.file_edit,
+          ["ctrl-t"] = actions.file_tabedit,
+          ["ctrl-v"] = actions.file_vsplit,
+          ["ctrl-s"] = actions.file_split,
+        },
+      },
+    })
+  end,
+  keys = {
+    {
+      "<leader>su",
+      function()
+        require("fzf-lua").files({
+          cwd = "Source",
+          prompt = "> ",
+          -- stylua: ignore
+          fd_opts = table.concat({
+            "--type", "f",
+            "--follow",
+            "--exclude", "Binaries",
+            "--exclude", "Intermediate",
+            "--exclude", "DerivedDataCache",
+            "--extension", "cpp",
+            "--extension", "h",
+          }, " "),
+        })
+      end,
+      desc = "[S]earch [U]nreal Source",
     },
   },
-  ---@diagnostic enable: missing-fields
 }
