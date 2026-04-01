1713c758aba48110244394071505c94c0a376572
qiekn <qiekndev@gmail.com>
2026-04-01 19:23:49 +0800

    nvim: treesitter → cpp20 module highlights

---
+++ .config/nvim/after/queries/cpp/highlights.scm
@@ -0,0 +1,14 @@
+; extends
+
+; for cpp20 modules
+(module_declaration
+  [
+    "export"
+    "module"
+  ] @keyword)
+
+(export_declaration
+  "export" @keyword)
+
+(import_declaration
+  "import" @keyword)
