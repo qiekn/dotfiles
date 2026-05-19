83cb701c87e55ad0d8a955c1f5ddfb4a1ac718b6
qiekn <qiekndev@gmail.com>
2026-05-20 07:30:28 +0800

    nvim: code-runner fix

--- .config/nvim/lua/plugins/runner.lua
+++ .config/nvim/lua/plugins/runner.lua
@@ -1,14 +1,24 @@
 return {
   "qiekn/code-runner.nvim",
+  keys = {
+    { "<F5>", "<cmd>w<CR><cmd>Run<CR>", desc = "Run current file", silent = true },
+    { "<F8>", "<cmd>ToggleTerm<CR>", desc = "Toggle terminal", silent = true },
+  },
   config = function()
     require("code-runner").setup({
       use_terminal = true, -- true: split terminal, false: :! mode
       term_height = 15,
+
+      run_scripts = {},
+
       cpp = {
         single_file_cmd = "clang++ -std=c++23 -stdlib=libc++ -o /tmp/{name} {file} && /tmp/{name}",
         src_dir = "src", -- source directory name
       },
-      -- filetype_cmds = {},
+      filetype_cmds = {
+        -- jai = "jai -quiet {file} && ./{name}.exe",
+        jai = "jai -quiet {file} >/tmp/jai.log 2>&1 && ./{name}.exe || cat /tmp/jai.log",
+      },
       keymaps = {
         toggle_term = "<leader>j", -- set to false to disable
       },
