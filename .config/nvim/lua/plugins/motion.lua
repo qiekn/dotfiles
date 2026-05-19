17970ac386a2e5bb0f0232fc97971672c8f23ee7
qiekn <qiekndev@gmail.com>
2026-05-20 07:33:29 +0800

    nvim: motion → zk (remove extra spaces from aligned code)

--- .config/nvim/lua/plugins/motion.lua
+++ .config/nvim/lua/plugins/motion.lua
@@ -10,7 +10,7 @@ return {
   },
   -- stylua: ignore
   keys = {
-    { "zk",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "zk",    mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
     { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
     { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
