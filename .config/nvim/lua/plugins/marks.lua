18c4874d36594c543dc7732b6e1ec45b1b8057a0
qiekn <qiekndev@gmail.com>
2026-05-31 05:57:50 +0800

    nvim: marks → do not hightlight marker line

--- .config/nvim/lua/plugins/marks.lua
+++ .config/nvim/lua/plugins/marks.lua
@@ -3,7 +3,8 @@ return {
   event = "VeryLazy",
   opts = {
     default_mappings = true,
-    builtin_marks = { ".", "<", ">", "^" },
+    -- builtin_marks = { ".", "<", ">", "^" },
+    builtin_marks = { "^" },
     cyclic = true,
     force_write_shada = false,
     refresh_interval = 250,
@@ -11,4 +12,16 @@ return {
     excluded_filetypes = { "neo-tree", "lazy", "mason" },
     mappings = {},
   },
+  config = function(_, opts)
+    require("marks").setup(opts)
+    -- disable line-number highlight on mark lines; keep only cursorline highlight
+    local function clear_mark_numhl()
+      vim.api.nvim_set_hl(0, "MarkSignNumHL", { link = "LineNr" })
+    end
+    clear_mark_numhl()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("MarksNumHLOverride", { clear = true }),
+      callback = clear_mark_numhl,
+    })
+  end,
 }
