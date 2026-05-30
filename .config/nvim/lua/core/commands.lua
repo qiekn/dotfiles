bd90d06ca8ae9212902f17a3e8f8123d835d8251
qiekn <qiekndev@gmail.com>
2026-05-31 05:57:15 +0800

    nvim: theme → change windows terminal background to sync with nvim theme

--- .config/nvim/lua/core/commands.lua
+++ .config/nvim/lua/core/commands.lua
@@ -1,3 +1,33 @@
 vim.cmd("command Q q!")
 vim.cmd("command Symbols Neotree document_symbols")
 vim.cmd("command Rmd RenderMarkdown toggle")
+
+-- When you open neovim, change current windows terminal background
+local function sync_terminal_bg()
+  local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+
+  if not normal.bg then
+    return
+  end
+
+  local bg = string.format("#%06x", normal.bg)
+
+  io.write(string.format("\27]11;%s\7", bg))
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = sync_terminal_bg,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = sync_terminal_bg,
+})
+
+-- When exit vim, reset windows terminal background color
+local original_bg = "#282c34"
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  callback = function()
+    io.write(string.format("\27]11;%s\7", original_bg))
+  end,
+})
